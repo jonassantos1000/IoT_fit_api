3d959952ef2d5a2eee9fe101f0dc1956aca14cef
Fabricio <fabriciorocha@magnasistemas.com.br>
2022-08-12 13:15:31 -0300

    Implementando dado de passos médios

--- model/response.py
+++ model/response.py
@@ -1,8 +1,9 @@
 class Response:
-    def __init__(self, passos, distancia, velocidade_media):
+    def __init__(self, passos, distancia, velocidade_media, passos_medio):
         self._passos = passos
         self._distancia = distancia
         self._velocidade_media = velocidade_media
+        self._passos_medio = passos_medio
 
     @property
     def passos(self):
@@ -15,6 +16,10 @@ class Response:
     @property
     def velocidade_media(self):
         return self._velocidade_media
+
+    @property
+    def passos_medio(self):
+        return self._passos_medio
 
     def dict(self):
         response = {}
