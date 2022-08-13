1e47b4b4f27e9eea0e810e6901d182b3dc5d1a42
fabricior0cha <fabriciorochapalmeira@gmail.com>
2022-08-13 13:57:27 -0300

    Feat:[main, index.html, helpers] Estruturando helpers e criando alerta de erro.

---
+++ static/js/helpers.js
@@ -0,0 +1,63 @@
+function instanciarGraficos() {
+    graficoDistancia = new GraficoLinha('grafico_distancia', options.DISTANCIA_TEMPO);
+    graficoPassos = new GraficoLinha('grafico_passos', options.PASSOS_METRO);
+}
+
+function criarTodosGraficos(){
+    instanciarGraficos();
+    graficoPassos.criarGrafico();
+    graficoDistancia.criarGrafico();
+}
+
+
+function desenharGraficos() {
+    google.charts.load('current', { 'packages': ['corechart'] });
+    google.charts.setOnLoadCallback(criarTodosGraficos);
+};
+
+function somarDistancia(distancia){
+    distanciaTotal += distancia
+    return distanciaTotal
+}
+
+function somarPassos(passos){
+    passosTotal += passos
+    return passosTotal
+}
+
+function atualizarTotalizadores() {
+    $("#total_passos").text(passosTotal)
+    $("#distancia_total").text(Math.floor(distanciaTotal))
+    $("#velocidade_maxima").text(velocidadeMaxima)
+}
+
+function atualizarVelocidadeMaxima(velocidade){
+    if(velocidadeMaxima <= velocidade){
+        velocidadeMaxima = velocidade
+    }
+    return velocidadeMaxima
+}
+
+function atualizarTodosGraficos(json){
+    graficoDistancia.atualizarDados([(segundos / 60),
+        Math.floor(json.distancia), `${json.velocidade_media} ${graficoDistancia._unidadeMedida}`])
+
+    graficoPassos.atualizarDados([somarDistancia(json.distancia), json.passos,
+                `${json.passos_medio} ${graficoPassos._unidadeMedida}`])
+}
+
+function mostrarAlerta(mensagem){
+     let alerta = $("#alerta")
+     let alertaParagrafo = $("#alerta p")
+     alertaParagrafo.text(mensagem)
+     alerta.show()
+     setTimeout(function() {
+        alerta.fadeOut('fast')
+        },  3000)
+}
+
+//Garante o que o gráfico seja responsivo
+$(window).resize(function(){
+  graficoDistancia.desenharGrafico()
+  graficoPassos.desenharGrafico()
+});
--- static/js/main.js
+++ static/js/main.js
@@ -6,42 +6,6 @@ let velocidadeMaxima = 0
 let segundos = 0
 
 
-function criarTodosGraficos() {
-    graficoDistancia = new GraficoLinha('grafico_distancia', options.DISTANCIA_TEMPO);
-    graficoPassos = new GraficoLinha('grafico_passos', options.PASSOS_METRO);
-
-    graficoPassos.criarGrafico();
-    graficoDistancia.criarGrafico();
-}
-
-function desenharGraficos() {
-    google.charts.load('current', { 'packages': ['corechart'] });
-    google.charts.setOnLoadCallback(criarTodosGraficos);
-};
-
-function somarDistancia(distancia){
-    distanciaTotal += distancia
-    return distanciaTotal
-}
-
-function somarPassos(passos){
-    passosTotal += passos
-    return passosTotal
-}
-
-function atualizarTotalizadores() {
-    $("#total_passos").text(passosTotal)
-    $("#distancia_total").text(Math.floor(distanciaTotal))
-    $("#velocidade_maxima").text(velocidadeMaxima)
-}
-
-function atualizarVelocidadeMaxima(velocidade){
-    if(velocidadeMaxima <= velocidade){
-        velocidadeMaxima = velocidade
-    }
-    return velocidadeMaxima
-}
-
 //Começa a contar o tempo de atividade
 setInterval(() => {
     segundos += 1
@@ -54,20 +18,15 @@ setInterval(() => {
     fetchDados()
         .then(response => response.json())
         .then(json => {
-            graficoDistancia.atualizarDados([(segundos / 60), Math.floor(json.distancia), `${json.velocidade_media} ${graficoDistancia._unidadeMedida}`])
-            graficoPassos.atualizarDados([somarDistancia(json.distancia), json.passos, `${json.passos_medio} ${graficoPassos._unidadeMedida}`])
-
+            atualizarTodosGraficos(json)
             atualizarVelocidadeMaxima(json.velocidade_media)
             somarPassos(json.passos)
             atualizarTotalizadores()
         })
-        .catch(err => console.log(err))
+        .catch(err => {
+            mostrarAlerta("Ocorreu um erro ao atualizar os dados.")
+        })
 }, 30000)
 
-//Garante o que o gráfico seja responsivo
-$(window).resize(function(){
-  graficoDistancia.desenharGrafico()
-  graficoPassos.desenharGrafico()
-});
 
 desenharGraficos()
